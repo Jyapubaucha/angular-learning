b3f66cb32fee1daaa6a55c3c1f1890429ec94276
Jyapubaucha <Jyapubaucha21@gmail.com>
2023-03-08 18:30:08 +0545

    Added strictPropertyInitialization to angular.json to correct error.

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit  { 
 
+  todos:Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'First ToDo',
+        desc: 'Learn Angular',
+        active: true
+      },
+      {
+        sno: 2,
+        title: 'Second ToDo',
+        desc: 'Learn React',
+        active: false
+      },
+      {
+        sno: 3,
+        title: 'Third ToDo',
+        desc: 'Learn Vue',
+        active: false
+      }
+    ]
+  }
+
+
+
+
+  ngOnInit(): void {
+    
+  }
 }
